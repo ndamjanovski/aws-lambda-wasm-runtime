d347dda2ec573fad72cc7fd86ef9499f3a62ab5f
ndamjanovski <nikola.damjanovski@wowinc.com.com>
2025-03-11 23:27:32 +0100

    rem next export to use output:export

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // ✅ Correct way in Next.js 15+
   basePath: '/aws-lambda-wasm-runtime',
 }
+
+module.exports = nextConfig;
